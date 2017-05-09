65727f6ad0a85fed29ebdbd9789df911dd6d1046
iwatakeshi <iwatakeshi@gmail.com>
2017-05-08 18:44:52 -0600

    Update: Complete Chapter 7

--- src/Lox.java
+++ src/Lox.java
@@ -7,7 +7,9 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+  private static final Interpreter interpreter = new Interpreter();
   static boolean hadError = false;
+  static boolean hadRuntimeError = false;
   public static void main(String[] args) throws IOException {
     try {
       if (args.length > 1) {
@@ -24,6 +26,7 @@ public class Lox {
     byte[] bytes = Files.readAllBytes(Paths.get(path));
     run(new String(bytes, Charset.defaultCharset()));
     if(hadError) System.exit(65);
+    if(hadRuntimeError) System.exit(70);
   }
 
   private static void runPrompt() throws IOException {
@@ -44,7 +47,7 @@ public class Lox {
     List<Token> tokens = scanner.scanTokens();
     Parser parser = new Parser(tokens);
     Expr expression = parser.parse();
-
+    interpreter.interpret(expression);
     if(!hadError) {
       System.out.println(new ASTPrinter().print(expression));
     }
@@ -58,6 +61,12 @@ public class Lox {
     if (token.type == TokenType.EOF) {
       report(token.line, " at end", message);
     } else report (token.line, " at '" + token.lexeme + "'", message);
+  }
+
+  static void runtimeError(RuntimeError error) {
+    System.err.println(error.getMessage() +
+        "\n[line " + error.token.line + "]");
+    hadRuntimeError = true;
   }
 
   private static void report(int line, String where, String message) {
---
+++ src/RuntimeError.java
@@ -0,0 +1,10 @@
+/**
+ * RuntimeError
+ */
+public class RuntimeError extends RuntimeException {
+  final Token token;
+  public RuntimeError (Token token, String message) {
+    super(message);
+    this.token = token;
+  }
+}
